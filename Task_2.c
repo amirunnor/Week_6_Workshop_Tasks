ef621e8eda2521c666edfc5447e08201e2e5d801
root <root@DESKTOP-80BHG2P>
2024-04-08 09:57:13 +0800

    coded the second task

--- Task_2.c
+++ Task_2.c
@@ -32,6 +32,8 @@ the data in the printed output is delimited using one whitespace character.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 struct node
 {
@@ -44,16 +46,26 @@ void insert_node(struct node** treePtr, int data);
 void inOrder(struct node* treePtr);
 void delete_tree(struct node** treePtr);
 
-int main() {
+int main(int argc, char *argv[]) {
+	if (argc < 1)
+      printf("Not enough command line arguments given!");
+    char input[50];
+    strcpy(input, argv[1]);
+	char* tokenPtr = NULL; //ptr to each token in the string
+
 	int temp = 0;
 	struct node* treePtr = NULL;
-    printf("Enter the value of the new data member: ");
-	scanf("%d", &temp);
-    while (temp > 0)
+    //printf("Enter the value of the new data member: ");
+	//scanf("%d", &temp);
+	tokenPtr = strtok(argv[1],",");
+	//temp = atoi(tokenPtr);
+    while (tokenPtr != NULL)
     {
-        insert_node(&treePtr, temp);
-        printf("Enter the value of the new data member: ");
-        scanf("%d", &temp);            
+        insert_node(&treePtr, atoi(tokenPtr));
+		tokenPtr = strtok(NULL,",");
+		//temp = atoi(tokenPtr);
+        //printf("Enter the value of the new data member: ");
+        //scanf("%d", &temp);            
     }
     printf("Initial version of binary tree:\n");
     inOrder(treePtr);
@@ -99,7 +111,11 @@ void inOrder(struct node* treePtr)
 
 void delete_tree(struct node** treePtr)
 {
-       free(*treePtr);
-	   delete_tree(&((*treePtr)->leftPtr));
-       delete_tree(&((*treePtr)->rightPtr));
+	if (*treePtr != NULL)
+	{
+		delete_tree(&((*treePtr)->leftPtr));//look left
+		delete_tree(&((*treePtr)->rightPtr));//look right
+		free(*treePtr);//visit node
+		*treePtr=NULL;// inform the tree that node has been freed
+	}
 }
